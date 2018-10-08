46813f06d3a378d605c13994cfcb41c06b2e4ddb
AlexWalker28 <alexwolk28@gmail.com>
2018-10-08 15:59:50 +0600

    Save new users to db

--- app/src/main/java/kg/kloop/android/openbudgetapp/MainActivity.java
+++ app/src/main/java/kg/kloop/android/openbudgetapp/MainActivity.java
@@ -1,22 +1,37 @@
 package kg.kloop.android.openbudgetapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.util.Log;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 100;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private FirebaseFirestore db;
+    private CollectionReference usersCollectionRef;
+    private DocumentReference userDocRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +44,60 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(new TendersFragmentPageAdapter(getSupportFragmentManager()));
         tabLayout.setupWithViewPager(viewPager);
 
+        db = FirebaseFirestore.getInstance();
+        usersCollectionRef = db.collection("users");
+        //add more in the future
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build());
 
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                addUserToDbIfNew(user);
+
+            }
+        }
+    }
+
+    private void addUserToDbIfNew(final FirebaseUser firebaseUser) {
+        userDocRef = db.document("users/" + firebaseUser.getUid());
+        userDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    if (!task.getResult().exists()) {
+                        User newUser = new User();
+                        newUser.setId(firebaseUser.getUid());
+                        newUser.setName(firebaseUser.getDisplayName());
+                        newUser.setPhotoUrl(String.valueOf(firebaseUser.getPhotoUrl()));
+                        usersCollectionRef.document(newUser.getId()).set(newUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.v(TAG, "User is saved to db");
+                                }
+                            }
+                        });
+
+                    } else {
+                        Log.v(TAG, "User is already in db");
+                    }
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/kg/kloop/android/openbudgetapp/User.java
@@ -0,0 +1,45 @@
+package kg.kloop.android.openbudgetapp;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String id;
+    private String name;
+    private String photoUrl;
+    private String role;
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
