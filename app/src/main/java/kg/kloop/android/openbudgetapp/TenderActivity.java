d48fa16981f0cc5d541eb46919a15c8861e843ec
AlexWalker28 <alexwolk28@gmail.com>
2018-10-11 21:21:31 +0600

    Show images for tender work with Glide

--- app/src/main/java/kg/kloop/android/openbudgetapp/TenderActivity.java
+++ app/src/main/java/kg/kloop/android/openbudgetapp/TenderActivity.java
@@ -11,10 +11,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -216,9 +218,15 @@ public class TenderActivity extends AppCompatActivity {
     }
 
     private void addWorkView(TenderTaskWork tenderTaskWork, ViewGroup layout) {
-        View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.tender_work, null);
+        View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.tender_work, (ViewGroup) layout.getRootView(), false);
         TextView textView = view.findViewById(R.id.tender_work_text_view);
         textView.setText(tenderTaskWork.getText());
+        if (tenderTaskWork.getPhotoUrl() != null) {
+            ImageView imageView = view.findViewById(R.id.tender_work_image_view);
+            Glide.with(this)
+                    .load(tenderTaskWork.getPhotoUrl())
+                    .into(imageView);
+        }
         layout.addView(view);
     }
 }
